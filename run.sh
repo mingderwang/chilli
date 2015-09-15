74f5ada723dcb360db7bdd2d89dd59f886599fc7
Ming-der Wang <ming@log4analytics.com>
2015-09-15 16:36:58 +0800

    move gcr to asia.gcr.io

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker build -f example/Dockerfile.asia.gcr.io -t gcr.io/mitac-cust-gcp-1/base-onion .
-docker build -t ming/chilli .
-docker run -d -p 8080:8080 ming/chilli
+docker build -f example/Dockerfile.asia.gcr.io -t asia.gcr.io/winter-wonder-647/base-onion .
+#docker build -t ming/chilli .
+#docker run -d -p 8080:8080 ming/chilli
+gcloud docker push asia.gcr.io/winter-wonder-647/base-onion
