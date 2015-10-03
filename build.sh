855b9f705aa9ec89e26e1c226ddea2664c350d39
Ming-der Wang <ming@log4analytics.com>
2015-10-03 12:41:31 +0800

    test

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -o xtrace
+#docker build --no-cache=true -t asia.gcr.io/winter-wonder-647/base-onion:latest example
 docker build -t asia.gcr.io/winter-wonder-647/base-onion:latest example
-#docker build -t ming/chilli .
-#docker run -d -p 8080:8080 ming/chilli
---
+++ testlocal.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -o xtrace
+docker build -t ming/chilli .
+docker run -d -p 8080:8080 ming/chilli
