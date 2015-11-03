499decd6b713f7ebc5c355132cce096b7951e42d
Ming-der Wang <ming@log4analytics.com>
2015-11-03 16:54:29 +0800

    add test bitcoin push

--- example/test.sh
+++ example/test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-curl -X POST -d @test.json --header "content-type: application/json" http://api.log4security.com:32060/port 
-
+for i in `seq 1 1`;
+do
+curl -X POST -d @kube.json --header "content-type: application/json" http://api.log4security.com:32569/kube 
+done
